7cf0968e9f94c2c0705edf14c310218b407e117a
Nickteca <131386769+Nickteca@users.noreply.github.com>
2025-03-27 22:11:58 -0600

    asdasdas

--- src/main/java/com/salespointfxadmin/www/controller/RecoleccionController.java
+++ src/main/java/com/salespointfxadmin/www/controller/RecoleccionController.java
@@ -14,6 +14,8 @@ import com.salespointfxadmin.www.model.SucursalRecoleccion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -21,8 +23,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyEvent;
 
- @Component
+@Component
 public class RecoleccionController implements Initializable {
 
 	@FXML
@@ -122,6 +125,97 @@ public class RecoleccionController implements Initializable {
 
 	}
 
+
+	
+    @FXML
+    void keytyped(KeyEvent event) {
+    	try {
+			TextField tf = (TextField) event.getSource();
+			// Verificar si el campo de texto no está vacío
+	        String text = tf.getText().trim();
+	        if (text.isEmpty()) {
+	            return;
+	        }
+
+
+			switch (tf.getId()) {
+			case "tFieldX1000": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 1000;
+				label1000.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX100": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 100;
+				label100.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX10": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 10;
+				label10.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX1": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 1;
+				label1.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX200": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 200;
+				label200.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX500": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 500;
+				label500.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX50": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 50;
+				label50.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX2": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 2;
+				label2.setText(subtotal + "");
+				break;
+			}
+			case "tFieldX5": {
+
+				int numero = Integer.parseInt(tf.getText());
+				int subtotal = numero * 5;
+				label5.setText(subtotal + "");
+				break;
+			}
+			default:
+				break;
+				//throw new NumberFormatException("No e sun numeor al parcer: " + tf.getId());
+			}
+		} catch (NumberFormatException e) {
+			Alert err = new Alert(AlertType.ERROR);
+			err.setTitle("error!!");
+			err.setHeaderText("No es numero al oparcer");
+			err.setContentText(e.getMessage() + "\n" + e.getCause());
+			err.show();
+			//e.printStackTrace();
+		}
+    }
+
 	@FXML
 	void registrar(ActionEvent event) {
 
@@ -131,35 +225,35 @@ public class RecoleccionController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		iniciarDataPicher();
 		numerosTexfiel();
-		
+
 	}
-	
+
 	private void iniciarDataPicher() {
 		dPickerFin.setValue(LocalDate.now());
 		dPickerInicio.setValue(LocalDate.now());
 	}
-	
+
 	private void numerosTexfiel() {
 		UnaryOperator<TextFormatter.Change> numericFilter = change -> {
-            // Asegura que la entrada solo contenga números (0-9) y se permita el cambio
-            String text = change.getText();
-            if (text.matches("[0-9]*")) {  // Solo acepta dígitos del 0 al 9
-                return change; // Si el texto es numérico, permite el cambio
-            }
-            return null; // Si no es numérico, no permite el cambio
-        };
+			// Asegura que la entrada solo contenga números (0-9) y se permita el cambio
+			String text = change.getText();
+			if (text.matches("[0-9]*")) { // Solo acepta dígitos del 0 al 9
+				return change; // Si el texto es numérico, permite el cambio
+			}
+			return null; // Si no es numérico, no permite el cambio
+		};
 
-        // Crear un TextFormatter con el filtro
-     // Aplicar el filtro a los TextFields ya definidos
-        tFieldX1.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX10.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX100.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX1000.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX2.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX200.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX5.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX50.setTextFormatter(new TextFormatter<>(numericFilter));
-        tFieldX500.setTextFormatter(new TextFormatter<>(numericFilter));
+		// Crear un TextFormatter con el filtro
+		// Aplicar el filtro a los TextFields ya definidos
+		tFieldX1.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX10.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX100.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX1000.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX2.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX200.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX5.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX50.setTextFormatter(new TextFormatter<>(numericFilter));
+		tFieldX500.setTextFormatter(new TextFormatter<>(numericFilter));
 	}
 
 }
